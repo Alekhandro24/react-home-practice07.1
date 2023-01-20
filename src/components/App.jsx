23cf4ef2683462eea57dca33ba6248f88ff54a37
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-01-20 20:24:13 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
+import {
+  useGetGoodsQuery,
+  useAddProductMutation,
+  useDeleteProductMutation,
+} from 'redux/index';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCouunt] = useState('');
+  const [newProduct, setNewProduct] = useState('');
+
+  // запрос на сервер за нашими даними, поверне об*єкт
+  const { data = [], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isError }] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct('');
+    }
+  };
+
+  const handleDeleteProduct = async id => {
+    await deleteProduct(id).unwrap();
+  };
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          value={newProduct}
+          onChange={e => setNewProduct(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={handleAddProduct}
+          style={{ cursor: 'pointer' }}
+        >
+          Add product
+        </button>
+      </div>
+      <div>
+        <select value={count} onChange={e => setCouunt(e.target.value)}>
+          <option value="">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            {item.name}
+            <span
+              onClick={() => handleDeleteProduct(item.id)}
+              style={{ cursor: 'pointer' }}
+            >
+              ⌫
+            </span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
-};
+}
+
+export default App;
